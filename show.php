ea80d8817aa3ad34bdfb1bbda2845ba86e05de0d
unknown <ellyn.vasquez@iudigital.edu.co>
2025-01-26 21:45:47 -0500

    otro commit

--- show.php
+++ show.php
@@ -84,40 +84,31 @@ function extractEmail($fromHeader)
 // Configuración
 $credentialsPath = 'credentials.json'; // Ruta al archivo JSON de credenciales
 $spreadsheetId = '1xuQVC9zlxLRJriWlPG3Y7py_O_0gfO5RMVF1U0XdTZc'; //Contacto
-$spreadsheetId = '13c8EjNZYI4Sq5wlQbvOTKVL4rLH_jXtsOxfrQrSAVMQ'; //ellyn.vasquez
+//$spreadsheetId = '13c8EjNZYI4Sq5wlQbvOTKVL4rLH_jXtsOxfrQrSAVMQ'; //ellyn.vasquez
 $startDate = '2025/01/24'; //YYYY/MM/DD
-$endDate = '2025/01/24'; //YYYY/MM/DD
+$endDate = '2025/01/25'; //YYYY/MM/DD
 $miEmail = 'ellyn.vasquez@iudigital.edu.co';
-$canalesIngresoArray = ['contacto@iudigital.edu.co', 'soportecampus@iudigital.edu.co','atencionalciudadano@iudigital.edu.co'];
+$canalesIngresoArray = ['contacto@iudigital.edu.co', 'soportecampus@iudigital.edu.co', 'atencionalciudadano@iudigital.edu.co'];
 $client = new Google_Client();
 $client->setAuthConfig($credentialsPath);
 $client->setAccessType('offline');
 $client->setPrompt('select_account consent');
 $client->addScope('https://www.googleapis.com/auth/gmail.readonly');
 $client->addScope('https://www.googleapis.com/auth/spreadsheets');
-
-
+$data = []; // Array para almacenar los datos extraídos
 try {
-    // Obtener el servicio de Gmail
     $gmailService = getGmailService($client);
-    // Obtener el servicio de Sheets
     $sheetsService = getSheetsService($client);
-
-    /* echo '<pre style="font-family: calibri; font-size: 11px">';
-    print_r($client->getAccessToken());
-    echo '</pre>'; */
-
+    $processedMessages = [];
     // Construir la consulta para mensajes leídos entre las fechas
-    $query = sprintf('label:read after:%s before:%s', $startDate, $endDate);
+    $query = sprintf('label:read');
+    //$query = sprintf('label:read after:%s before:%s', $startDate, $endDate);
 
     // Obtener los mensajes que coincidan con la consulta
-    $messages = $gmailService->users_messages->listUsersMessages('me', ['maxResults' => 2]);
-
-
-    // Obtener los mensajes
-    //$messages = $gmailService->users_messages->listUsersMessages('me', ['maxResults' => 5]);
-
-    $data = []; // Array para almacenar los datos extraídos
+    $messages = $gmailService->users_messages->listUsersMessages('me', [
+        'q' => $query,    // Aplicar la consulta
+        'maxResults' => 200 // Número máximo de mensajes a obtener
+    ]);
 
     $labels = $gmailService->users_labels->listUsersLabels('me');
     $userLabels = [];
@@ -125,97 +116,114 @@ try {
         $userLabels[$label->getId()] = $label->getName();
     }
 
-
-    foreach ($messages->getMessages() as $message) {
+    foreach ($messages->getMessages() as $cont => $message) {
+        echo "Procesando mensaje " . ($cont + 1) . " de " . count($messages->getMessages()) . ' <br>';
         $msg = $gmailService->users_messages->get('me', $message->getId());
-        $headers = $msg->getPayload()->getHeaders();
+        // Obtener el ID del mensaje
+        //$messageId = $msg->getId();
 
 
-        $messageId = $msg->getId(); // ID del mensaje
-        $messageUrl = "https://mail.google.com/mail/u/0/#inbox/" . $messageId;
 
 
-        $timestamp = $msg->getInternalDate();
+        $threadId = $message->getThreadId();
+
+
+        // Obtener todos los mensajes del hilo utilizando el threadId
+        $thread = $gmailService->users_threads->get('me', $threadId);
+        $firstMessage = null;
         $from = $subject = $receivedDate = $receivedTime = "";
 
-        $originalSender = ''; // Variable para almacenar el correo original
+        // Iterar sobre los mensajes del hilo para encontrar el primero
+        foreach ($thread->getMessages() as $msgth) {
+            // Obtener el timestamp (fecha interna) del mensaje
+            $timestamp = $msgth->getInternalDate();
 
-        foreach ($headers as $header) {
-            // Obtener el remitente principal
-            if ($header->getName() === 'From') {
-                $from = $header->getValue();
-            }
-            if ($header->getName() == 'Subject') {
-                $subject = $header->getValue();
-            }
-
-            // Intentar encontrar el correo original a través de "X-Forwarded-For"
-            if ($header->getName() === 'X-Forwarded-For') {
-                $originalSender = $header->getValue();
-            }
-
-            // O también verificar el campo "Received"
-            if ($header->getName() === 'Received') {
-                // Aquí puedes intentar extraer la dirección del remitente original si está presente
-                if (preg_match('/from (.*?) /', $header->getValue(), $matches)) {
-                    $originalSender = $matches[1]; // Usar la dirección que aparece en el encabezado "Received"
-                }
-            }
+            // Agregar cada mensaje con su fecha al array
+            $threadMessages[] = [
+                'message' => $msgth,
+                'timestamp' => $timestamp
+            ];
         }
+        // Ordenar los mensajes por fecha (internalDate) de manera ascendente
+        usort($threadMessages, function ($a, $b) {
+            return $a['timestamp'] <=> $b['timestamp']; // Ordenar de menor a mayor
+        });
 
-        if(in_array($from, $canalesIngresoArray)){
-            $canalIngreso = extractEmail($from);
-        }
-
-        $timestamp = $msg->getInternalDate(); // Marca de tiempo en milisegundos
-
-        // Crear un objeto DateTime con la marca de tiempo
-        $dateTime = new DateTime();
-        $dateTime->setTimestamp($timestamp / 1000); // Convertir milisegundos a segundos
-
-        // Establecer la zona horaria a la local (por ejemplo, Bogotá)
-        $dateTime->setTimezone(new DateTimeZone('America/Bogota'));
-
-        // Formatear la fecha y la hora
-        $receivedDate = $dateTime->format('Y-m-d'); // Fecha en formato AAAA-MM-DD
-        $receivedTime = $dateTime->format('H:i:s'); // Hora en formato HH:mm:ss
-
-        /** INICIO:Ver el ultimo correo respondido */
-        $threadId = $msg->getThreadId();
-        $thread = $gmailService->users_threads->get('me', $threadId);
-
-        foreach ($thread->getMessages() as $messageThread) {
-            $fechaSolucion = '';
-            $horaSolucion = '';
-            $headersThread = $messageThread->getPayload()->getHeaders();
-            $fromThread = '';
-            $dateThread = '';
-
-            // Extraer los encabezados relevantes
-            foreach ($headersThread as $headerThread) {
-                if ($headerThread->getName() === 'From') {
-                    $fromThread = $headerThread->getValue();
-                }
-                if ($headerThread->getName() === 'Date') {
-                    $dateThread = $headerThread->getValue();
-                }
+        $firstMessage = $threadMessages[0];
+        // Ahora $firstMessage contiene el primer correo del hilo
+        if ($firstMessage) {
+            // Extraer los detalles del primer correo
+            $msgth = $firstMessage['message'];
+            $headers = $msgth->getPayload()->getHeaders();
+            $messageId = $msgth->getId(); // ID del primer mensaje
+            $messageUrl = "https://mail.google.com/mail/u/0/#inbox/" . $messageId;
+            $specificUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSc9Nl_BHQ9JdKF6W8LxJcGN9dk3WB7uD7aTtT6-2xFsV4wT7g/viewform';
+            // Verificar si el mensaje ya ha sido procesado (comprobando el messageId)
+            if (in_array($messageId, $processedMessages)) {
+                continue; // Si ya procesamos este mensaje, saltamos al siguiente
             }
 
-            // Verifica si el mensaje fue enviado por ti
-            if (strpos($fromThread, $miEmail) !== false) {
-                // Ajustar la fecha y hora
-                $dateTimeRe = new DateTime($dateThread);
-                $dateTimeRe->setTimezone(new DateTimeZone('America/Bogota')); // Ajusta la zona horaria si es necesario
+            $processedMessages[] = $messageId;
+            echo '<pre>';
+            print_r($processedMessages);
+            echo '</pre>';
+
+            // Obtener los encabezados del primer mensaje
+            foreach ($headers as $header) {
                
-                $fechaSolucion = $dateTimeRe->format('Y-m-d');
-                $horaSolucion = $dateTimeRe->format('H:i:s');
+                // Buscar el campo "From"
+                if ($header->getName() == 'From') {
+                    $from = extractEmail($header->getValue());  // Obtiene el valor del encabezado "From"
+                }
+                // Buscar el campo "Subject"
+                if ($header->getName() == 'Subject') {
+                    $subject = $header->getValue();
+                }
+            }
 
+            $timestamp = $msgth->getInternalDate(); // Marca de tiempo en milisegundos
+            // Crear un objeto DateTime con la marca de tiempo
+            $dateTime = new DateTime();
+            $dateTime->setTimestamp($timestamp / 1000); // Convertir milisegundos a segundos
+
+            // Establecer la zona horaria a la local (por ejemplo, Bogotá)
+            $dateTime->setTimezone(new DateTimeZone('America/Bogota'));
+
+            // Formatear la fecha y la hora
+            $receivedDate = $dateTime->format('Y-m-d'); // Fecha en formato AAAA-MM-DD
+            $receivedTime = $dateTime->format('H:i:s'); // Hora en formato HH:mm:ss
+            $FechaTotalInicio = $dateTime->format('Y-m-d H:i:s'); // Obtener solo la hora
+
+
+            if (in_array($from, $canalesIngresoArray)) {
+                $canalIngreso = $from;
+            } else {
+                $canalIngreso = $canalesIngresoArray[0];
+            }
+            echo $from;
+            if ($from == 'atencionalciudadano@iudigital.edu.co') {
+               echo $originalBody = $msgth->getPayload()->getBody()->getData();
+
+               
             }
         }
 
-        /**FIN: */
 
+        // El último mensaje será el último en el array ordenado
+        $lastMessage = $threadMessages[count($threadMessages) - 1];
 
+        // Obtener la fecha y hora del último mensaje
+        $lastTimestamp = $lastMessage['timestamp'];
+        $lastDateTime = new DateTime();
+        $lastDateTime->setTimestamp($lastTimestamp / 1000); // Convertir milisegundos a segundos
+        $lastDateTime->setTimezone(new DateTimeZone('America/Bogota')); // Ajustar a tu zona horaria
+        $FechaTotalFin = $lastDateTime->format('Y-m-d H:i:s'); // Obtener solo la hora
+        $fechaInicio = new DateTime($FechaTotalInicio);  // Convertir string a DateTime
+        $fechaFin = new DateTime($FechaTotalFin);        // Convertir string a DateTime
+
+        // Calcular la diferencia entre las fechas
+        $intervalo = $fechaInicio->diff($fechaFin);
+        $tiempoSolucion = $intervalo->format('%d días, %h:%i:%s');
 
         $labelIds = $msg->getLabelIds();
 
@@ -228,10 +236,13 @@ try {
             }
         }
 
+        $currentTime = new DateTime();
+        $currentTime->setTimezone(new DateTimeZone('America/Bogota'));
+        $formattedDate = $currentTime->format('Y-m-d H:i:s');
         // Crear fila
         $row = [
-            $timestamp ?? "", //Marca temporal
-            extractEmail($from) ?? "", //Correo electronico
+            $formattedDate ?? "", //Current datetime
+            $from ?? "", //Correo electronico
             $receivedDate ?? "", //Fecha de recibido
             $receivedTime ?? "", //Hora de recibido
             getInterestedLabel($emailLabels, '*Agente') ?? "", //AGENTE //etiqueta
@@ -254,7 +265,7 @@ try {
             $horaSolucion ?? "", //Hora de solucion
             $canalIngreso ?? "", //Canal de Ingreso
             $encuesta ?? "", //Se envio Encuesta
-            $HoraRecibido ?? "", //Hora de recibido
+            $receivedTime ?? "", //Hora de recibido
             $FechaTotalInicio ?? "", //Fecha total de inicio
             $FechaTotalFin ?? "", //Fecha total de fin
             $tiempoSolucion ?? "", //Fecha total de fin
@@ -301,7 +312,6 @@ try {
     ];
     $data['values'] = array_merge([$headersTable], $data['values'] ?? []);
 
-
     // Crear el rango en Sheets
     $range = 'Hoja 1!A1';
     $body = new ValueRange(['values' => $data['values']]);
@@ -309,22 +319,6 @@ try {
     $params = ['valueInputOption' => 'RAW'];
     $sheetsService->spreadsheets_values->update($spreadsheetId, $range, $body, $params);
 
-
-    // Imprimir los datos
-    /* echo '<pre style="font-family: calibri; font-size: 11px">';
-    print_r($data);
-    echo '</pre>'; */
-
-    // Crear el rango en el que insertar los datos
-    $range = 'Hoja 1!A2';
-
-    $body = new ValueRange([
-        'values' => $data
-    ]);
-
-    // Escribir los datos en la hoja de cálculo
-    $params = ['valueInputOption' => 'RAW'];
-    $sheetsService->spreadsheets_values->update($spreadsheetId, $range, $body, $params);
 
     echo "Datos insertados correctamente en Google Sheets.";
 } catch (Exception $e) {
